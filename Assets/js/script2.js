27401a5762d026c6c9c1c6656133e4d2524e53ce
Ryan Evans <rdevans87@gmail.com>
2021-02-21 16:30:46 -0500

    logging scores in local storage isse

--- Assets/js/script2.js
+++ Assets/js/script2.js
@@ -3,29 +3,29 @@ var highScore = document.querySelector("#highScore");
 var clearScore = document.querySelector("#clearScore");
 var BackToquiz = document.querySelector("#backBtn");
 
-function render(storedScores) {
-
 var storedScores = localStorage.getItem ("storedScores")
 storedScores = JSON.parse(storedScores);
 
-if (storedScores !== null) {
+if (storedScores !== true) {
 
-for (var i = 0; 1 < storedScores.length; i++) {
+for (var i = 0; 1 < storedScore.length; i++) {
 
     var liElement = document.createElement("li");
-    liElement.textContent = storedScores[i].initials + " " + storedScores[i].score;
-    highScore.appendChild(liElement);
+    liElement.textContent = storedScores[i].userInput + " " + storedScores[i].finalScore;
+    highScores.appendChild(liElement);
 }
 
 }
 
-//Event listener to clear scores 
 clearScore.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
 
-}
+
+
+
+
 
 
 
